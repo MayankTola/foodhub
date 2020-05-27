a54478726c8858c7f8cdbfced60a3dc2b0df6db7
Mayank Tola <tolamayank546@gmail.com>
2020-05-28 00:46:16 +0530

    Fine tuned the login system

--- accounts/views.py
+++ accounts/views.py
@@ -11,23 +11,11 @@ def register(request):
     return render(request, '../templates/register.html')
 
 
-# def customer_register(request):
-#     form = CustomerSignUpForm(request.POST or None)
-#     if form.is_valid():
-#         user = form.save()
-#         password = form.cleaned_data.get("password")
-#         user.set_password(password)
-#         user.save()
-#         new_user = authenticate(username=user.username, password=password)
-#         # login(request, new_user)
-#         return redirect("/")
-#     return render(request, "login/customer_register.html", {
-#         "form": form,
-#     })
 class customer_register(CreateView):
     model = User
     form_class = CustomerSignUpForm
-    template_name = 'login/customer_register.html'
+    template_name = 'login/login_form.html'
+    extra_context = {'title': "Customer Registration"}
 
     def form_valid(self, form):
         user = form.save()
@@ -38,30 +26,14 @@ class customer_register(CreateView):
 class restaurant_register(CreateView):
     model = User
     form_class = RestaurantSignUpForm
-    template_name = 'login/restaurant_register.html'
+    template_name = 'login/login_form.html'
+    extra_context = {'title': "Restaurant Registration"}
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
         return redirect('/')
 
-
-# def login_request(request):
-#     if request.method == 'POST':
-#         form = AuthenticationForm(data=request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('/')
-#             else:
-#                 messages.error(request, "Invalid username or password")
-#         else:
-#             messages.error(request, "Invalid username or password")
-#     return render(request, 'login/login_form.html',
-#                   context={'form': AuthenticationForm()})
 
 def login_request(request):
     form = UsersLoginForm(request.POST or None)
@@ -72,7 +44,7 @@ def login_request(request):
         login(request, user)
         return redirect("/")
     return render(request, "login/login_form.html", {
-        "form": form,
+        "form": form, "title": "Log In"
     })
 
 
